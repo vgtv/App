7942f0157ba98c6a1516a80c265f674e5e952e07
Steffen Andre Marstein <s305488@stud.hioa.no>
2018-03-20 15:20:38 +0100

    HEU

--- App/Ng5/src/app/app.module.ts
+++ App/Ng5/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule, MatInputModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
