75bae9b009ea3076e22a2dbcfb6fc7a44d44872f
Julian Heimonen Hjallum <s305501@stud.hioa.no>
2018-03-22 13:03:20 +0100

    Fikset wordcloud

--- App/Ng5/src/app/app.module.ts
+++ App/Ng5/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
