ebbc2df3ff7211b8f190cdd227be2038e7928436
an2n <an2n@DESKTOP-PPKS5BV>
2018-04-04 14:04:56 +0200

    Oppdatert julian feil

--- App/Ng5/src/app/scatter/scatter.component.ts
+++ App/Ng5/src/app/scatter/scatter.component.ts
@@ -36,8 +36,7 @@ export class ScatterComponent {
               backgroundColor: 'transparent',
               title: 'Publikasjoner vs. kvalitet',
               hAxis: {
-                title: 'Kvalitet',
-                ticks: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+                title: 'Kvalitet'
               },
               vAxis: { title: 'Publikasjoner' },
               legend: 'none',
